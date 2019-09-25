c59156b97d1d850a45446beecaf1f41613ed2ca6
Jorge Dari <jorgedari71@gmail.com>
2019-09-24 23:20:07 -0300

    Primera version

--- js/funcMeses.js
+++ js/funcMeses.js
@@ -1,10 +1,13 @@
   'use strict'
     alert('Estoy en la funcion ahora !!!!');
     document.getElementById("btn").addEventListener('click',ejecutar);
-  
-     function ejecutar()
-     {
+
+    function ejecutar(){
       let mes = document.getElementById("elMes").Value;
       mes=parseInt(mes);
       alert(mes);
+
     }
+      
+  
+   
