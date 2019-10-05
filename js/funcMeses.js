6c0bc792bd2e6daacdfa53657caed5fc0ddd7ecd
Jorge Dari <jorgedari71@gmail.com>
2019-10-05 07:51:57 -0300

    tercera version

--- js/funcMeses.js
+++ js/funcMeses.js
@@ -1,19 +1,11 @@
-  'use strict'
-    var arreglo = ['HTML',15,true];
-    console.log (arreglo);
-    var objeto = {
-      nombre: 'html',
-      duracion: 15,
-      estado: true
-    }
-    console.log (objeto.nombre);
+ 
+    alert('jorge');
 
     document.getElementById("btn").addEventListener('click',ejecutar);
 
     function ejecutar(){
       let mes = document.getElementById("elMes").Value;
-      mes=parseInt(mes);
-      alert(mes);
+      console.log(mes);
 
     }
       
